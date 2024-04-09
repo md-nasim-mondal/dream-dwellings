dca0166e6fb32787d777262ec604990c96915cdd
Nasim <nasim.programmer24.com>
2024-04-09 11:23:20 +0600

    made user profile photo dynamic and set menu bar icon and close bar icon dynamic for small devices

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import userDefaultPic from "../../../assets/user.png";
-import { IoMenu } from "react-icons/io5";
+import { IoClose, IoMenu } from "react-icons/io5";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
     const { user, logOut } = useContext(AuthContext);
+    const photoURL = user?.photoURL;
+    const displayName = user?.displayName;
 
     const handleSignOut = () => {
         logOut().then().catch();
@@ -34,13 +37,16 @@ const Navbar = () => {
                     <div className="dropdown">
                         <div
                             tabIndex={0}
+                            onClick={()=> setOpen(!open)}
                             role="button"
                             className="btn-ghost lg:hidden">
-                            <IoMenu />
+                            {open ? <IoClose className="text-2xl" /> : <IoMenu className="text-2xl" />}
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            className={`menu menu-sm dropdown-content
+                                ${open ? '' : 'hidden'}
+                                mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52`}>
                             {navLinks}
                         </ul>
                     </div>
@@ -51,31 +57,35 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-end">
                     {user ? (
-                        <>
+                        <div className="flex items-center">
                             <div
+                                className="tooltip tooltip-left"
+                                data-tip={displayName}>
+                                <div
                                 tabIndex={0}
                                 role="button"
                                 className="btn btn-ghost btn-circle avatar">
-                                <div className="w-10 rounded-full">
+                                <div className="w-12 rounded-full">
                                     <img
                                         alt="Tailwind CSS Navbar component"
-                                        src={userDefaultPic}
+                                        src={photoURL || userDefaultPic}
                                     />
                                 </div>
                             </div>
-                            <button onClick={handleSignOut} className="btn">
+                            </div>
+                            <button onClick={handleSignOut} className="btn btn-sm md:btn-md  btn-neutral ml-2">
                                 Sign Out
                             </button>
-                        </>
+                        </div>
                     ) : (
                         <>
                             <Link to={"/login"}>
-                                <button className="btn btn-outline btn-primary mr-0.5 md:mr-4">
+                                <button className="btn btn-xs md:btn-md btn-outline btn-primary mr-0.5 md:mr-4">
                                     Login
                                 </button>
                             </Link>
                             <Link to={"/register"}>
-                                <button className="btn btn-outline btn-primary">
+                                <button className="btn btn-xs md:btn-md btn-outline btn-primary">
                                     Register
                                 </button>
                             </Link>
